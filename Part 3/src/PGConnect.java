f9b7ce774dbdee57b3844e6f589de2ef5d062f0e
Nathan Bingham <scatterplot@outlook.com>
2015-03-04 19:37:56 -0800

    Update
    Includes Part 3

---
+++ Part 3/src/PGConnect.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * @author Nathan Bingham
+ * @date March 4, 2015
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class PGConnect
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/teammembers","student","student");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM teammembers");
+            
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getInt("ID")+": "+resultSet.getString("firstName")+" "+resultSet.getString("lastName")+" - "+resultSet.getString("jobTitle"));
+            }
+        }
+        catch (SQLException sqle)
+        {
+            System.err.println(sqle.getMessage());
+        }
+    }
+    
+}
